841dbdcd62d715cfc84eead0eed93805c64dd421
16nabila <nabilasarkar169148@gmail.com>
2022-12-23 01:40:24 +0600

    SP LAB WORK 5

---
+++ SP lab work 5/10.Binary half pxrimid.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows = 5;
+
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            if(j % 2 == 1)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("1");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
